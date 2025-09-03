2fb5c51a545175ae3f7a2242fcf48663a19edd01
Asif-vai <mr3366533@gmail.com>
2025-09-03 12:38:09 +0600

    added skill section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Skills from './components/Skills';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Skills/>
     </div>
   );
 }
